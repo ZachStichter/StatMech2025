a9634dd6daee0c96fbb4391f3b6ef6609e2af593
ZachStichter <zstichter@gmail.com>
2025-04-23 23:48:47 -0400

    added several constants

--- constants.py
+++ constants.py
@@ -1,3 +1,6 @@
 k_b = 1 # set constants to 1 for easy simulation
 m = 1 # mass of particle
-T = 1 # Arbitrary Temp. units
+T = 10 # Arbitrary Temp. units
+dt = 0.001 # simulation timestep arbitrary units
+friction = 0.01 # simulation friction force
+steps = 10000 # number of simulation steps
